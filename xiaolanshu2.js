242567795f6f377953300c2785bd2c71e27afd28
chenglich <cheng-lich@outlook.com>
2023-12-27 21:39:12 +0800

    Update xiaolanshu2.js

--- xiaolanshu2.js
+++ xiaolanshu2.js
@@ -1,14 +1,16 @@
 
 [rewrite_local]
 
-https?:\/\/api\.u\.ccb\.com\/website\/player\/studypointview url script-response-body https://raw.githubusercontent.com/chenglich/qxrewrite/main/xiaolanshu2.js
+https?:\/\/api\.u\.ccb\.com\/website\/player\/studypointview url script-response-body
+https://raw.githubusercontent.com/chenglich/qxrewrite/main/xiaolanshu2.js
 
 [mitm] 
 
 hostname=api.u.ccb.com
 
 ***********************************/
+console.log($response.body);
 var body = $response.body.replace('0,', '1,');
 console.log(body);
 
-$done(body);
+$done({ body });
