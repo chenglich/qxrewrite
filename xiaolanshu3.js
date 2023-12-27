042f0f3264ed2ff4c2e0f927d23d2feb98e24397
chenglich <cheng-lich@outlook.com>
2023-12-27 22:50:33 +0800

    Update xiaolanshu3.js

--- xiaolanshu3.js
+++ xiaolanshu3.js
@@ -1,6 +1,6 @@
 /**********/
 var obj=JSON.parse($request.body);
-console.log(obj);
+console.log(obj['pointListMap']);
 obj['pointListMap']=obj['pointListMap'].map(item => item === 0 ? 1 : item);
-console.log(obj);
+console.log(obj['pointListMap']);
 $done({body:JSON.stringify(obj)});
