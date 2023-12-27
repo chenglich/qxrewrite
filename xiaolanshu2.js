d28c087e1dad0e2d6dfbefe6de9256632d95a250
chenglich <cheng-lich@outlook.com>
2023-12-27 21:19:09 +0800

    Create xiaolanshu2.js

---
+++ xiaolanshu2.js
@@ -0,0 +1,13 @@
+
+[rewrite_local]
+
+https?:\/\/api\.u\.ccb\.com\/website\/player\/studypointview url script-response-body https://raw.githubusercontent.com/chenglich/qxrewrite/main/xiaolanshu2.js
+
+[mitm] 
+
+hostname=api.u.ccb.com
+
+***********************************/
+var body = $response.body.replace(/0,/g, '1,')
+
+$done({ body })
