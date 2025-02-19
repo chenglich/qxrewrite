88714e92d6b8e2c33e4335e0a5b07cfad0f562c1
chenglich <cheng-lich@outlook.com>
2025-02-19 18:18:33 +0800

    Update complex_rewrite.js

--- complex_rewrite.js
+++ complex_rewrite.js
@@ -24,82 +24,82 @@ const $ = new Env("qxr");
   });
 
 /**************************************
-function signin() {
-  return new Promise((resolve) => {
-    const header = {
-      Accept: `application/json, text/plain, */*`,
-      Origin: `https://glados.rocks`,
-      "Accept-Encoding": `gzip, deflate, br`,
-      Cookie: sicookie,
-      "Content-Type": `application/json;charset=utf-8`,
-      Host: `glados.rocks`,
-      Connection: `keep-alive`,
-      "User-Agent": `Mozilla/5.0 (iPhone; CPU iPhone OS 14_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1`,
-      'Authorization': siauthorization,
-      "Accept-Language": `zh-cn`,
-    };
-    const body = `{ "token": "glados.one" }`;
-    const signinRequest = {
-      url: "https://glados.rocks/api/user/checkin",
-      headers: header,
-      body: body,
-    };
-    $.post(signinRequest, (error, response, data) => {
-      var body = response.body;
-      var obj = JSON.parse(body);
-      if (obj.message != "oops, token error") {
-        if (obj.message != "Please Try Tomorrow") {
-          var date = new Date();
-          var y = date.getFullYear();
-          var m = date.getMonth() + 1;
-          if (m < 10) m = "0" + m;
-          var d = date.getDate();
-          if (d < 10) d = "0" + d;
-          var time = y + "-" + m + "-" + d;
-          var business = obj.list[0].business;
-          var sysdate = business.slice(-10);
-          if (JSON.stringify(time) == JSON.stringify(sysdate)) {
-            change = obj.list[0].change;
-            changeday = parseInt(change);
-            message += `今日签到获得${changeday}天`;
-          } else {
-            message += `今日签到获得0天`;
-          }
-        } else {
-          message += "今日已签到";
-        }
-      } else {
-        message += obj.message;
-      }
-      resolve();
-    });
-  });
-}
+// function signin() {
+//   return new Promise((resolve) => {
+//     const header = {
+//       Accept: `application/json, text/plain, */*`,
+//       Origin: `https://glados.rocks`,
+//       "Accept-Encoding": `gzip, deflate, br`,
+//       Cookie: sicookie,
+//       "Content-Type": `application/json;charset=utf-8`,
+//       Host: `glados.rocks`,
+//       Connection: `keep-alive`,
+//       "User-Agent": `Mozilla/5.0 (iPhone; CPU iPhone OS 14_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1`,
+//       'Authorization': siauthorization,
+//       "Accept-Language": `zh-cn`,
+//     };
+//     const body = `{ "token": "glados.one" }`;
+//     const signinRequest = {
+//       url: "https://glados.rocks/api/user/checkin",
+//       headers: header,
+//       body: body,
+//     };
+//     $.post(signinRequest, (error, response, data) => {
+//       var body = response.body;
+//       var obj = JSON.parse(body);
+//       if (obj.message != "oops, token error") {
+//         if (obj.message != "Please Try Tomorrow") {
+//           var date = new Date();
+//           var y = date.getFullYear();
+//           var m = date.getMonth() + 1;
+//           if (m < 10) m = "0" + m;
+//           var d = date.getDate();
+//           if (d < 10) d = "0" + d;
+//           var time = y + "-" + m + "-" + d;
+//           var business = obj.list[0].business;
+//           var sysdate = business.slice(-10);
+//           if (JSON.stringify(time) == JSON.stringify(sysdate)) {
+//             change = obj.list[0].change;
+//             changeday = parseInt(change);
+//             message += `今日签到获得${changeday}天`;
+//           } else {
+//             message += `今日签到获得0天`;
+//           }
+//         } else {
+//           message += "今日已签到";
+//         }
+//       } else {
+//         message += obj.message;
+//       }
+//       resolve();
+//     });
+//   });
+// }
 
-function status() {
-  return new Promise((resolve) => {
-    const statusRequest = {
-      url: "https://glados.rocks/api/user/status",
-      headers: { Cookie: sicookie },
-    };
-    $.get(statusRequest, (error, response, data) => {
-      var body = response.body;
-      var obj = JSON.parse(body);
-      if (obj.code == 0) {
-        account = obj.data.email;
-        expday = obj.data.days;
-        remain = obj.data.leftDays;
-        remainday = parseInt(remain);
-        message += `\n已用${expday}天,剩余${remainday}天`;
-        $.msg("GLaDOS", `账户：${account}`, message);
-      } else {
-        $.log(response);
-        $.msg("GLaDOS", "", "❌请重新登陆更新Cookie");
-      }
-      resolve();
-    });
-  });
-}
+// function status() {
+//   return new Promise((resolve) => {
+//     const statusRequest = {
+//       url: "https://glados.rocks/api/user/status",
+//       headers: { Cookie: sicookie },
+//     };
+//     $.get(statusRequest, (error, response, data) => {
+//       var body = response.body;
+//       var obj = JSON.parse(body);
+//       if (obj.code == 0) {
+//         account = obj.data.email;
+//         expday = obj.data.days;
+//         remain = obj.data.leftDays;
+//         remainday = parseInt(remain);
+//         message += `\n已用${expday}天,剩余${remainday}天`;
+//         $.msg("GLaDOS", `账户：${account}`, message);
+//       } else {
+//         $.log(response);
+//         $.msg("GLaDOS", "", "❌请重新登陆更新Cookie");
+//       }
+//       resolve();
+//     });
+//   });
+// }
 **************************************/
   
 function handle_req_body() {
